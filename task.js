62deceb6f776fc5ff542d4a7d652535a578fcfba
Shalaevskiy Nikolay <nshalaevskiy@intraservices.ru>
2024-06-17 11:43:10 +0300

    final

--- task.js
+++ task.js
@@ -2,10 +2,10 @@ let timer = document.getElementById('timer');
 
 let count = Number(timer.textContent);
 
-setInterval(function () {
+let timerId = setInterval(function () {
   timer.textContent = count;
   if (--count < 0) {
-    clearInterval(this);
+    clearInterval(timerId);
     alert('Вы победили в конкурсе');
   }
 }, 1000);
